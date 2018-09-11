6b2c3fdd8584bb5d2eb119c8f21011b3c98ff677
RickyMendez <21ricky@gmail.com>
2018-09-10 22:50:56 -0600

    modify layout for reviews and services sections, start top navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,1 @@
-const navItems = document.querySelectorAll('.nav-item');
-let menuToggle = false;
 
-navItems.forEach(item => item.addEventListener('click', () => {
-  if (!menuToggle) {
-    item.children[1].classList.add('show');
-    menuToggle = true;
-  } else {
-    item.children[1].classList.remove('show');
-    menuToggle = false;
-  }
-}));
